1820560a61c0cd9303b428ea312d304aa7c0a637
Juan Martin Marco <jmmarco@gmail.com>
2017-09-20 14:59:55 -0300

    Hookup react router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { Provider } from 'react-redux'
+import { Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import configureStore from './store/configureStore'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
@@ -9,7 +11,12 @@ import registerServiceWorker from './registerServiceWorker'
 const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'));
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route path="/" component={App}/>
+      </BrowserRouter>
+    </Provider>
+
+, document.getElementById('root'));
 registerServiceWorker();
