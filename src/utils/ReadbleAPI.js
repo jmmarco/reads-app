642c420856efeb458269d2481978a579b7bacce3
Juan Martin Marco <jmmarco@gmail.com>
2017-09-25 16:46:59 -0300

    Export api and headers to simplify

--- src/utils/ReadbleAPI.js
+++ src/utils/ReadbleAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+export const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
 
 // Generate a unique token for storing the Posts data on the backend server
 let token = localStorage.token
@@ -6,14 +6,8 @@ let token = localStorage.token
 if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8)
 
-const headers = {
-  'Accept': 'application/json',
+// Switch to 'Content-Type: 'application/json', so it works with the Udacity API
+export const headers = {
+  'Content-Type': 'application/json',
   'Authorization': token
 }
-
-export const getCategories = () =>
-  fetch(`${api}/categories`, {
-    headers
-  })
-  .then(res => res.json())
-  .then(data => console.log(data))
