0b6490d5f84b42c42705c82a3039e560baf5b223
Juan Martin Marco <jmmarco@gmail.com>
2017-09-25 11:58:43 -0300

    Wire voting buttons

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import ArrowUp from 'react-icons/lib/fa/arrow-circle-o-up'
+import ArrowDown from 'react-icons/lib/fa/arrow-circle-o-down'
 
 class Post extends Component {
+
+  upVote = (e) => {
+    e.preventDefault()
+    console.log("Upvote was clicked!")
+  }
+
+  downVote = (e) => {
+    e.preventDefault()
+    console.log("Downvote was clicked!")
+  }
 
   render() {
     const { post } = this.props
@@ -14,10 +26,20 @@ class Post extends Component {
           <h2>{post.title}</h2>
           <h3>Posted in category: <Link to={`/categories/${post.category}`}>{post.category}</Link></h3>
           <p>{post.body}</p>
+
           <hr/>
           <p>Written by: <a href="">{post.author}</a> on: {new Date(post.timestamp).toLocaleString()}</p>
-          <button>Edit</button>
-          <button>Remove</button>
+          <p>Score: {post.voteScore}</p>
+
+          <div className="controls">
+            <button>Edit</button>
+            <button>Remove</button>
+            <span className="vote-control">
+              <button onClick={this.upVote}><ArrowUp size={30}/></button>
+              <button onClick={this.downVote}><ArrowDown size={30}/></button>
+            </span>
+
+          </div>
         </div>
       )
     }
