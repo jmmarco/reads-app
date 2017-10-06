147fef2927f301c88e82e6c95e8ca83dab1e1b11
Juan Martin Marco <jmmarco@gmail.com>
2017-10-06 14:15:10 -0300

    Add edit post reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,7 +3,8 @@ import {
   POSTS_IS_LOADING,
   POSTS_FETCH_SUCCESS,
   POST_UPVOTE,
-  POST_DOWNVOTE
+  POST_DOWNVOTE,
+  UPDATE_POST_SUCCESS
 } from '../actions/posts'
 
 // Remember a reducer takes in two things:
@@ -31,7 +32,10 @@ export function postsIsLoading(state = false, action) {
 export function posts(state = [], action) {
 
   // object destructuring from action
-  const { id, voteScore } = action
+  const { id } = action
+
+  // console.log(action)
+  // console.log(id, voteScore)
 
   // Declare some helper variables
   let postToUpdate, stateWithoutPost, score
@@ -40,6 +44,7 @@ export function posts(state = [], action) {
     case POSTS_FETCH_SUCCESS:
       return action.posts
     case POST_UPVOTE:
+      console.log("Payload is:", action)
       stateWithoutPost = state.filter((post) => post.id !== id )
       postToUpdate = state.find((post) => post.id === id)
 
@@ -58,6 +63,23 @@ export function posts(state = [], action) {
       return [
         postToUpdate,
         ...stateWithoutPost
+      ]
+    case UPDATE_POST_SUCCESS:
+      console.log("Payload is:", action)
+      console.log(
+        "HEllo",
+        // ...state.find((post) => post.id === id),
+      )
+      console.log(
+        "hello below is: ",
+        ...state.filter((post) => post.id !== id ),
+        "hello above is: ",
+        // Object.assign({}, action.post)
+
+      )
+      return [
+        ...state.filter((post) => post.id !== id ),
+        // Object.assign({}, action.post)
       ]
     default:
       return state
