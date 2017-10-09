31e504bae9433b1e1362c5f89758dcfefd7273d4
Juan Martin Marco <jmmarco@gmail.com>
2017-10-09 16:54:10 -0300

    Add sort by score and date

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,9 +2,68 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Category extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: null,
+      order: true,
+    }
+
+    this.toggleSort = this.toggleSort.bind(this)
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      posts: this.props.posts
+    })
+  }
+
+
+  toggleSort(type) {
+    let posts = this.state.posts
+    let _ = require('lodash')
+
+
+
+    let sortedPosts = null
+
+    if (type === 'score') {
+      switch (this.state.order) {
+        case true:
+        sortedPosts = _.orderBy(posts, 'voteScore', 'asc')
+        this.setState({ order: false  })
+        break
+        default:
+        sortedPosts = _.orderBy(posts, 'voteScore', 'desc')
+        this.setState({ order: true })
+      }
+    }
+
+    if (type === 'date') {
+      switch (this.state.order) {
+        case true:
+        sortedPosts = _.orderBy(posts, 'timestamp', 'asc')
+        this.setState({ order: false  })
+        break
+        default:
+        sortedPosts = _.orderBy(posts, 'timestamp', 'desc')
+        this.setState({ order: true })
+      }
+    }
+
+    // Finally, set the state
+    this.setState({
+      posts: sortedPosts
+    })
+  }
+
+
   render() {
-    const { category, posts } = this.props
-    if (category !== undefined) {
+    const { category } = this.props
+    const posts = this.state.posts
+
+    if (category !== undefined && posts!== null) {
       return (
         <div>
           <h2>These are all the posts in the {category.name} category</h2>
@@ -13,13 +72,19 @@ class Category extends Component {
             { posts.map((p) => {
               return (
                 <li key={p.id}>
-                  <Link to={`/posts/${p.id}`}>{p.title}</Link>
+                  <Link to={`/posts/${p.id}`}>{p.title} </Link>
+
+                  <span>- Score: {p.voteScore} | Date: {new Date(p.timestamp).toDateString()}</span>
                 </li>
               )
             })
 
             }
           </ul>
+          <hr/>
+          <button className="button-sort" onClick={() => this.toggleSort('score')}>Sort by score</button>
+          <button className="button-sort" onClick={() => this.toggleSort('date')}>Sort by date</button>
+
         </div>
       )
     }
