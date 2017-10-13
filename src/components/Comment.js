647c24bbff91a7d48201a3ff2a87ca77bfb2afd0
Juan Martin Marco <jmmarco@gmail.com>
2017-10-13 16:24:03 -0300

    Integrate add comment form component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,22 +1,73 @@
 import React, { Component } from 'react'
-import { fetchComments, upVote, downVote, deleteComment } from '../actions/comments'
+import { fetchComments, upVote, downVote, deleteComment, updateComment } from '../actions/comments'
 import { connect } from 'react-redux'
 import ArrowUp from 'react-icons/lib/fa/arrow-circle-o-up'
 import ArrowDown from 'react-icons/lib/fa/arrow-circle-o-down'
 import Edit from 'react-icons/lib/fa/edit'
 import Trash from 'react-icons/lib/fa/trash'
+import EditCommentForm from './EditCommentForm'
 
 class Comment extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isEditing: false,
+      comment: null
+    }
+
+
+    this.toggleEdit = this.toggleEdit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
   componentDidMount() {
     const { post } = this.props
     this.props.fetchComments(post.id)
   }
 
+  toggleEdit(comment) {
+    console.log("e is: ", comment)
+    this.setState({
+      comment: comment,
+      isEditing: !this.state.isEditing,
+    })
+    console.log(this.state)
+  }
+
+  handleChange(event) {
+    const field = event.target.name
+    const comment = this.state.comment
+
+    comment[field] = event.target.value
+
+    return this.setState({
+      comment: comment
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.updateComment(this.state.comment)
+    this.setState({
+      isEditing: false
+    })
+
+  }
+
 
   render() {
 
     const { comments } = this.props
+    const { comment } = this.state
+    console.log("Comment is now: ", this.state.comment)
+
+    if (this.state.isEditing && comment !== null) {
+      return (
+        <EditCommentForm comment={this.state.comment} handleChange={this.handleChange.bind(this)} handleSubmit={this.handleSubmit.bind(this)} toggleEdit={this.toggleEdit.bind(this)}/>
+        )
+    }
 
     if (this.props.comments) {
       return (
@@ -33,7 +84,7 @@ class Comment extends Component {
                     <button onClick={() => this.props.deleteComment(comment)}>
                       <Trash size={25}/>
                     </button>
-                    <button>
+                    <button onClick={() => this.toggleEdit(comment)}>
                       <Edit size={25}/>
                     </button>
                     <button
@@ -77,6 +128,7 @@ function mapDispatchToProps (dispatch) {
   return {
     upVote: (data) => dispatch(upVote(data)),
     downVote: (data) => dispatch(downVote(data)),
+    updateComment:(data) => dispatch(updateComment(data)),
     deleteComment: (data) => dispatch(deleteComment(data)),
     fetchComments: (data) => dispatch(fetchComments(data))
   }
