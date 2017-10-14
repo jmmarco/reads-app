5d27436f23618fc9f91e0de0e5349f9b5e4ad835
Juan Martin Marco <jmmarco@gmail.com>
2017-10-13 21:01:28 -0300

    Improve edit Post form

--- src/components/EditPostForm.js
+++ src/components/EditPostForm.js
@@ -7,20 +7,34 @@ class EditPostForm extends Component {
     console.log("Props for EditPostForm are: ", this.props)
     const { post, handleSubmit, handleChange} =  this.props
     return(
-      <form onSubmit={handleSubmit}>
-        <label>
-          Title:
-          <input
-            type="text"
-            name="title"
-            value={post.value}
-            onChange={handleChange}
-            placeholder={post.title}
-          />
-        </label>
-        <br/>
-        <input type="submit" value="Submit"/>
-      </form>
+      <div>
+        <h2>Edit Post</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={post.value}
+              onChange={handleChange}
+              placeholder={post.title}
+            />
+          </label>
+          <label>
+            Body:
+            <input
+              type="text"
+              name="body"
+              value={post.value}
+              onChange={handleChange}
+              placeholder={post.body}
+            />
+          </label>
+          <br/>
+          <button type="submit">Submit Post</button>
+          <button>Go Back</button>
+        </form>
+      </div>
 
     )
 
