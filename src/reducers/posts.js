baaa5a27f78ccbbcae9ba262fd5909bda3a8792f
Juan Martin Marco <jmmarco@gmail.com>
2017-11-16 11:56:54 -0300

    Finally fixed sorting mechanism

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -36,11 +36,10 @@ export function postsIsLoading(state = false, action) {
 }
 
 export function posts(state = [], action) {
-
-  let _ = require('lodash')
+  let sorted = []
 
   // object destructuring from action
-  const { id,  posts } = action
+  const { id } = action
 
   // Declare some helper variables
   let postToUpdate, stateWithoutPost, score
@@ -83,25 +82,27 @@ export function posts(state = [], action) {
         ...state.concat(action.post)
       ]
       case SORT_POSTS_BY_DATE_ASC:
-        console.log('order by date ascending.. ',_.orderBy(posts, ['timestamp'], ['asc']))
-        return [...state,
-          _.orderBy(posts, ['timestamp'], ['asc'])
-        ]
+        sorted = state.slice().sort(function(a, b) {
+          return a.timestamp - b.timestamp
+        })
+        return sorted
       case SORT_POSTS_BY_DATE_DSC:
-        console.log('order by date descending.. ', _.orderBy(posts, ['timestamp'], ['dsc']))
-        return [...state,
-          _.orderBy(posts, ['timestamp'], ['dsc'])
-        ].reverse()
+        sorted = state.slice().sort(function(a, b) {
+          return b.timestamp - a.timestamp
+        })
+        return sorted
       case SORT_POSTS_BY_SCORE_ASC:
-        console.log(_.orderBy(posts, ['voteScore'], ['asc']))
-        return [...state,
-          _.orderBy(posts, ['voteScore'], ['asc'])
-        ]
+        sorted = state.slice().sort(function(a, b) {
+          return a.voteScore - b.voteScore
+        })
+        console.log(sorted)
+        return sorted
       case SORT_POSTS_BY_SCORE_DSC:
-        console.log(_.orderBy(posts, ['voteScore'], ['dsc']))
-        return [...state,
-          _.orderBy(posts, ['voteScore'], ['dsc'])
-        ].reverse()
+        sorted = state.slice().sort(function(a, b) {
+          return b.voteScore - a.voteScore
+        })
+        console.log(sorted)
+        return sorted
     default:
       return state
   }
